4b3deb44e8e977c47e86fa27eb0be7fddeb99b1d
viktor.chukhra <viktor.chukhra@axiomatics.com>
2015-12-12 21:45:19 +0200

    Expose contacts via REST. Add testing capability.

---
+++ src/main/java/com/fibo/contactmanager/web/ContactRestController.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2011, Axiomatics AB, Sweden.
+ * All rights reserved.
+ * <p/>
+ * This software is the confidential and proprietary information of Axiomatics AB.
+ */
+package com.fibo.contactmanager.web;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.fibo.contactmanager.domain.Contact;
+import com.fibo.contactmanager.service.ContactService;
+
+/**
+ * @author Viktor Chuhra
+ */
+@Controller
+@RequestMapping("/rest")
+public class ContactRestController {
+
+    @Autowired
+    private ContactService contactService;
+
+
+    @RequestMapping(value = "/contacts", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    List<Contact> listContacts() {
+        return contactService.listContact();
+
+    }
+
+   /* @RequestMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addContact(@ModelAttribute("contact") Contact contact,
+                             BindingResult result) {
+
+        contactService.addContact(contact);
+
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/delete/{contactId}")
+    public String deleteContact(@PathVariable("contactId") Integer contactId) {
+
+        contactService.removeContact(contactId);
+
+        return "redirect:/index";
+    }*/
+}
--- src/test/H2Test.java
+++ src/test/H2Test.java
@@ -2,22 +2,31 @@ import javax.persistence.PersistenceContext;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import com.fibo.contactmanager.dao.ContactDAO;
+import com.fibo.contactmanager.domain.Contact;
 import static org.junit.Assert.assertNotNull;
 
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-        "file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+        "file:src/main/webapp/WEB-INF/spring/test-context.xml"})
 public class H2Test {
 
     @PersistenceContext
     private javax.persistence.EntityManager em;
 
+    @Autowired
+    private ContactDAO contactDAO;
+
+
     @Before
     public void init() {
+        System.identityHashCode(this);
         assertNotNull(em);
 //        em.persist(dcd);
 //        em.flush();
@@ -25,8 +34,56 @@ public class H2Test {
     }
 
     @Test
-    public void testGet() {
-//        DataCollectionDefinition found = em.find(DataCollectionDefinition.class, 1);
-//        Assert.assertEquals(found, dcd);
+    @Rollback(true)
+    public void testStore() {
+        Contact contact = new Contact();
+        contact.setEmail("ddd");
+        contact.setFirstname("firstName");
+        contact.setLastname("lastName");
+        contactDAO.addContact(contact);
+
+////        Session session = new Configuration().configure("hibernate.cfg.xml")
+////                                             .buildSessionFactory()
+////                                             .openSession();
+////        em.getTransaction().begin();
+//
+//        ArrayList<String> list1 = new ArrayList<String>();
+//        list1.add("java is a programming language");
+//        list1.add("java is a platform");
+//
+//        ArrayList<String> list2 = new ArrayList<String>();
+//        list2.add("Servlet is an Interface");
+//        list2.add("Servlet is an API");
+//
+//        Question question1 = new Question();
+//        question1.setQname("What is Java?");
+//        question1.setAnswers(list1);
+//
+//        Question question2 = new Question();
+//        question2.setQname("What is Servlet?");
+//        question2.setAnswers(list2);
+//
+//        em.persist(question1);
+//        em.persist(question2);
+//
+//        Query query = em.createQuery("from Question");
+//        List<Question> list = (List<Question>) query.getResultList();
+//
+//        Iterator<Question> itr = list.iterator();
+//        while (itr.hasNext()) {
+//            Question q = itr.next();
+//            System.out.println("Question Name: " + q.getQname());
+//
+//            //printing answers
+//            List<String> list3 = q.getAnswers();
+//            Iterator<String> itr2 = list3.iterator();
+//            while (itr2.hasNext()) {
+//                System.out.println(itr2.next());
+//            }
+//        }
+//
+//        //  em.commit();
+//        // session.close();
+//        System.out.println("success");
     }
 }
